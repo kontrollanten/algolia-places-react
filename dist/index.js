df4e83599f23a50d7969be0910c5b557a0553085
kontrollanten <alexander@width.se>
2018-01-27 17:50:46 +0000

    build package

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _AlgoliaPlaces = require('./AlgoliaPlaces');
+
+var _AlgoliaPlaces2 = _interopRequireDefault(_AlgoliaPlaces);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _AlgoliaPlaces2.default;
