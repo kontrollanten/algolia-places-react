e14b0916e9d539febc4d3bedde6c8df12a802fcf
kontrollanten <alexander@width.se>
2018-02-25 00:36:00 +0100

    demo: add fork me on GH

--- e2e/App.js
+++ e2e/App.js
@@ -91,6 +91,7 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<Provider>
+        <a href="https://github.com/kontrollanten/algolia-places-react"><img style={{ position: 'absolute', top: 0, right: 0, border: 0 }} src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" /></a>
         <Heading pb={40}>Algolia Places React component in action</Heading>
 				<Flex mx={-2}>
 					<Box width={1/3} px={2}>
