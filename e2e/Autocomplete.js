71848fcf0bf9f254c124c44612a9793a2da1f455
kontrollanten <alexander@width.se>
2018-02-25 01:14:24 +0100

    demo: split code to multiple components

---
+++ e2e/Autocomplete.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AlgoliaPlaces from '../src';
+
+const Autocomplete = ({ onCallback }) => {
+  const handleChange = (data) => {
+    onCallback({
+      name: 'onChange',
+      args: data,
+    });
+    console.log('Fired when suggestion selected in the dropdown or hint was validated.');
+  };
+
+  const handleSuggestions = (data) => {
+    onCallback({
+      name: 'onSuggestions',
+      args: data,
+    });
+    console.log('Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed.');
+  };
+
+  const handleCursorChanged = (data) => {
+    onCallback({
+      name: 'onCursorChanged',
+      args: data,
+    });
+    console.log('Fired when arrows keys are used to navigate suggestions.');
+  };
+
+  const handleClear = () => {
+    onCallback({
+      name: 'onClear',
+      args: null,
+    });
+    console.log('Fired when the input is cleared.');
+  };
+
+  const handleLimit = (data) => {
+    onCallback({
+      name: 'onLimit',
+      args: data,
+    });
+    console.log('Fired when you reached your current rate limit.');
+  };
+
+  const handleError = (data) => {
+    onCallback({
+      name: 'onError',
+      args: data,
+    });
+    console.log('Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit.');
+  };
+
+  return (
+    <AlgoliaPlaces
+      placeholder="Write an address here"
+      onChange={handleChange}
+      onSuggestions={handleSuggestions}
+      onCursorChanged={handleCursorChanged}
+      onClear={handleClear}
+      onLimit={handleLimit}
+      onError={handleError}
+    />
+  );
+};
+
+Autocomplete.propTypes = {
+  onCallback: PropTypes.func.isRequired,
+};
+
+export default Autocomplete;
+
---
+++ e2e/FiredCallbacks.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import JsonView from 'react-json-view';
+import { Subhead, Panel } from 'rebass';
+
+const FiredCallbacks = ({ callbacks }) => (
+  <div>
+    <Subhead>Fired callbacks:</Subhead>
+    {callbacks
+      .map((cb, index, arr) => (
+        <Panel
+          color="blue"
+          key={(arr.length - index).toString()}
+          mt={20}
+        >
+          <Panel.Header
+            bg="blue"
+            color="white"
+          >
+            {cb.name}
+          </Panel.Header>
+          <JsonView
+            src={cb.args}
+            collapsed={1}
+            name={false}
+            enableClipboard={false}
+          />
+        </Panel>
+      ))}
+  </div>
+);
+
+FiredCallbacks.propTypes = {
+  callbacks: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default FiredCallbacks;
