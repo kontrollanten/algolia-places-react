b9ba781e5c864a05764f564311eb449b3d4a6526
kontrollanten <stambanan@pm.me>
2019-07-23 21:17:56 +0200

    chore(eslint): fix config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,17 +14,19 @@ module.exports = {
     "indent": ["error", 2],
     "react/destructuring-assignment": 0,
   },
-  "overrides": {
-    "files": ["*.test.js"],
-    "globals": {
-      "Event": true,
-      "KeyboardEvent": true,
-      "MouseEvent": true,
-      "window": true,
+  "overrides": [
+    {
+      "files": ["*.test.js"],
+      "globals": {
+        "Event": true,
+        "KeyboardEvent": true,
+        "MouseEvent": true,
+        "window": true,
+      },
+      "rules": {
+        "no-console": 0,
+        "no-empty": 0,
+      },
     },
-    "rules": {
-      "no-console": 0,
-      "no-empty": 0,
-    },
-  }
+  ],
 };
